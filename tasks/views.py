cf6fa1940c14b99a440e23427b018d140fa1581f
Likeson Saviyo <likesonsavio@gmail.com>
2026-07-06 01:10:28 +0530

    Added JWT authentication with djangoframework-simplejwt

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .serializers import taskSerializer
 from .models import Task
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 # Create your views here.
 
 class TaskView(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('-created_at')
     serializer_class = taskSerializer
+    permission_classes = [permissions.IsAuthenticated]
